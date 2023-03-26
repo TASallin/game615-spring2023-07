67f4b3572b3bf93190ecd75c846973e2ff684806
SenseiCanada <117216802+SenseiCanada@users.noreply.github.com>
2023-03-26 10:40:59 -0400

    added front/back door animations

---
+++ exercise07/Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, 80 * hAxis * Time.deltaTime, 0);
+    }
+}
